b183b019d0177766910b919e340c55756cd5f455
Amirali Karimi <itsral.karimi@gmail.com>
2022-11-25 00:15:42 +0330

    added some test file and configuration

---
+++ src/app/todo/todo.component.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+import { TodoComponent } from './todo.component';
+
+describe('TodoComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        TodoComponent
+      ],
+    }).compileComponents();
+  });
+});
